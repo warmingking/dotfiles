4ae9d28ff78a84118c46969a6ffd6390abc01cb3
zapeng <zhiang.peng@yitu-inc.com>
2022-09-14 20:25:44 +0800

    add shortcut for text search

--- private_dot_config/nvim/lua/configs/c_telescope.lua
+++ private_dot_config/nvim/lua/configs/c_telescope.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_keymap('n', '<Leader>ff',  [[<Cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>gf',  [[<Cmd>lua require('telescope.builtin').git_files()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>fg',  [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
